4b140afb1b447c39c8993027156f22c336a5cbe4
Emmie Drummond <emmiedrummondtextiledesign@gmail.com>
2024-11-27 20:11:40 +0000

    Organize project structure and update scripts

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CS5002_P3
+
+READ ME FILE_Next, create a new directory containing a plain
+ text file called README.txt or README.md where you will later add basic details about your project
--- initialscript.py
+++ initialscript.py
@@ -1,19 +1,38 @@
-
+import sys 
+import csv
+import json
 import pandas as pd
 import numpy as np
-import matplotlib
+import matplotlib.pyplot as plt 
+from pandas import DataFrame, Series 
+
 print("Hello World!")
 
+#four loop on pandas mapping
+#pin thing in sys 
+
 #https://blog.devgenius.io/5-essential-data-quality-checks-you-can-perform-with-python-18fc87655950
+#https://medium.com/@swaratvaghela30112003/working-with-csv-and-json-in-python-fc88e49c1c1d
 
-df = pd.read_csv('Scotland_teaching_file_1PCT.csv')
-#missing_values = df.isnull().sum()
-print(df)
 
-#duplicate_rows = df[df.duplicated()]
-#print(duplicate_rows)
+#df = pd.read_csv('data\Scotland_teaching_file_1PCT.csv')
 
-#df.drop_duplicates(inplace=True)
+df = "data/Scotland_teaching_file_1PCT.csv"
+data = pd.read_csv(df)
+with open("data\data_dictionary.json", "r") as jsonfile:
+    data_dictionary = json.load(jsonfile)
+
+#df = pd.DataFrame(np.array(data_dictionary)),
+
+print("Missing values:\n", data.isnull().sum())
+
+data_cleaned= data.drop_duplicates(inplace=True)
+#for columns in  df:  #convert csv to string? 
+
+
+#def columns
+
+#def rows
 
 
 #df = pd.DataFrame(np.array([[1, 2, 3, 1], [4, 5, 6, 0], [7, 8, 9, 0], [4, 5, 6, 1], [1, 2, 3, 1]]),
@@ -24,3 +43,18 @@ print(df)
 #to_keep = df[df.groupby(['a', 'b', 'c'])['result'].transform('nunique') == 1] #duplicates
 
 #print(to_keep)
+
+
+#read cvs file
+# use data_dictionary as 
+
+#first step:  the values of variables are of the expected format (numbers, strings, etc.)
+# the values of variables are admissible (e.g. are within a given range or are from the list of admissible values)
+
+#in case of any inconsistencies and/or duplicates found, produce new file with refined data to be used in the subsequent analysis;
+
+#duplicate_rows = df[df.duplicated()]
+#print(duplicate_rows)
+#df.drop_duplicates(inplace=True)
+
+#print(sys.argv) # this step must be automated to the point when it can be run with a single shell command to call an executable Python script specifying necessary argument(s);
