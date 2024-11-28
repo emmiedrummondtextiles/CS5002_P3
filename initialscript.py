467a628bca174b5ba05e1202e3a6d2b8efd41839
Emmie Drummond <emmiedrummondtextiledesign@gmail.com>
2024-11-28 20:46:41 +0000

    Cleaning data with pandas cheat sheet

--- initialscript.py
+++ initialscript.py
@@ -14,12 +14,23 @@ print("Hello World!")
 #https://blog.devgenius.io/5-essential-data-quality-checks-you-can-perform-with-python-18fc87655950
 #https://medium.com/@swaratvaghela30112003/working-with-csv-and-json-in-python-fc88e49c1c1d
 
+#read cvs file
+# use data_dictionary as 
+
+#first step:  the values of variables are of the expected format (numbers, strings, etc.)
+# the values of variables are admissible (e.g. are within a given range or are from the list of admissible values)
+#in case of any inconsistencies and/or duplicates found, produce new file with refined data to be used in the subsequent analysis;
+
 
 #df = pd.read_csv('data\Scotland_teaching_file_1PCT.csv')
 
-df = "data/Scotland_teaching_file_1PCT.csv"
-data = pd.read_csv(df)
-with open("data\data_dictionary.json", "r") as jsonfile:
+df = pd.read_csv("data/Scotland_teaching_file_1PCT.csv") #making json and csv different 
+print(df.dtypes)
+
+df_cleaned= df.drop_duplicates(inplace=True) 
+
+
+with open(r"data\data_dictionary.json", "r") as jsonfile: #forgot r
     data_dictionary = json.load(jsonfile)
 
 #df = pd.DataFrame(np.array(data_dictionary)),
@@ -31,9 +42,12 @@ print("Missing values:\n", data.isnull().sum())
 #print(duplicate_rows)
 #df.drop_duplicates(inplace=True)
 
-data_cleaned= data.drop_duplicates(inplace=True)
+#https://www.w3schools.com/python/pandas/pandas_cleaning.asp
 
-assert data_cleaned{"Age"}.between(0,120).all(),"out of range"
+
+#associate directly to data dictionary?, 
+
+assert data_cleaned["Age"].between(0,120).all(),"out of range" #object is not scriptable, Age
 
 #for columns in  df:  #convert csv to string? 
 
@@ -52,15 +66,5 @@ assert data_cleaned{"Age"}.between(0,120).all(),"out of range"
 
 
 #print(to_keep)
-
-
-#read cvs file
-# use data_dictionary as 
-
-#first step:  the values of variables are of the expected format (numbers, strings, etc.)
-# the values of variables are admissible (e.g. are within a given range or are from the list of admissible values)
-
-#in case of any inconsistencies and/or duplicates found, produce new file with refined data to be used in the subsequent analysis;
-
 
 #print(sys.argv) # this step must be automated to the point when it can be run with a single shell command to call an executable Python script specifying necessary argument(s);
